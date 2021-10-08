cdaa2bddec9f5e26528ca1a610f303ccfb58158d
enderNT <gayagocr@gmail.com>
2021-10-08 08:22:44 -0500

    Tarea resuelta

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -246,7 +246,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(letra!=1){
+  if(letra.length>1){
     return "Dato incorrecto";
   }
   switch(letra){
