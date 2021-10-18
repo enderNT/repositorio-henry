29e7c61de798340252134ff374d8f7d12cafde16
enderNT <gayagocr@gmail.com>
2021-10-18 14:29:03 -0500

    master

--- hola.js
+++ hola.js
@@ -1,10 +1,14 @@
-let obj = {
-  precio: 500,
-  porcentajeDeDescuento: 50,
-  calcularPrecioDeDescuento: function calculoDescuento() {
-    this.precio - (this.precio * (this.porcentajeDeDescuento / 100));
-    return obj.calcularPrecioDeDescuento;
-  },
-};
-
-console.log(obj.calcularPrecioDeDescuento);
+class Usuario {
+    constructor(usuario, nombre, email, password) {
+        this.usuario = usuario;
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+    }
+    saludar() {
+        return "Hola, mi nombre es " + this.nombre;
+    }
+}
+  
+let mariachi=new Usuario('juanin125', 'Juan', 'juan@gmail.com', 'juan1234_')
+console.log(mariachi.saludar()); 
